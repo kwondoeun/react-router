51d10c14368fe3df47157ec56cbec8cc3740ff0b
권도은 <kounduk9119t@naver.com>
2023-01-19 16:38:41 +0900

    2023-01-19

---
+++ src/component/BoardContent.js
@@ -0,0 +1,17 @@
+import { useParams } from "react-router-dom";
+
+
+const BoardContent = () => {
+    //구조분해 할당
+    let {num} = useParams()
+
+    return (
+
+        <div>
+            <h6>글 상세페이지..</h6>
+            {num}번 글 내용 ~~
+        </div>
+    )
+}
+
+export default BoardContent;
